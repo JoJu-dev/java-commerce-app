0dc4cfd183419983f9573c1f09d5997cd5ffdca7
Jose Figueroa <taodejoju@gmail.com>
2026-01-27 16:13:59 -0600

    Agregando test para probar credenciales (acceso) al login

---
+++ src/test/java/code/figueroa/models/login/LoginTest.java
@@ -0,0 +1,52 @@
+package code.figueroa.models.login;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+
+
+public class LoginTest {
+	
+	private String USER = "prueba";
+	private String PASSWORD = "12345";
+	
+	@Test
+	@DisplayName("input- Contraseña y password output contraseña o password incorrecto.")
+	void CasoDePruebaLogin(){
+		
+		var credenciales = new Login("prueba", "111111111");
+		
+		assertEquals(false,credenciales.acceso(USER, PASSWORD),"Prueba no debe admitir password");
+	}
+	@Test
+	@DisplayName("input- Contraseña y password output contraseña o password incorrecto.")
+	void CasoDePruebaUserIncorrecto(){
+		
+		var credenciales = new Login("pruebas", "12345");
+		
+		assertEquals(false,credenciales.acceso(USER, PASSWORD),"Prueba no debe admitir user");
+	}
+	
+	@Test
+	@DisplayName("input- Contraseña y password output contraseña o password incorrecto.")
+	void CasoDePruebaCredencialesIncorrectas(){
+		
+		var credenciales = new Login("pruebas", "s12345");
+		
+		assertEquals(false,credenciales.acceso(USER, PASSWORD),"Prueba no debe admitir Credenciales");
+	}
+	
+	@Test
+	@DisplayName("input- Contraseña y password output contraseña o password incorrecto.")
+	void CasoDePruebaCredencialesCorrectas(){
+		
+		var credenciales = new Login(USER, PASSWORD);
+		
+		assertEquals(true,credenciales.acceso(USER, PASSWORD),"Prueba  debe admitir Credenciales");
+	}
+
+
+}
